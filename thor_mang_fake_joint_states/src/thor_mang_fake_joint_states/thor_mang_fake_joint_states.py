a3d7bfc1b8ace00426fcc0b6bd2d09821502727d
Hector OCS Primary <hector-drc@gmx.com>
2015-06-03 12:07:54 -0700

    added missing joint

--- thor_mang_fake_joint_states/src/thor_mang_fake_joint_states/thor_mang_fake_joint_states.py
+++ thor_mang_fake_joint_states/src/thor_mang_fake_joint_states/thor_mang_fake_joint_states.py
@@ -13,11 +13,11 @@ class FakeJointStates(object):
 
 
     def __init__(self):
-        self._all_joints = ['head_pan', 'head_tilt', 'l_ankle_pitch', 'l_ankle_roll', 'l_elbow', 'l_f0_j0', 'l_f1_j0', 'l_hip_pitch', 'l_hip_roll', 'l_hip_yaw', 'l_knee', 'l_shoulder_pitch', 'l_shoulder_roll', 'l_shoulder_yaw', 'l_wrist_roll', 'l_wrist_yaw1', 'r_ankle_pitch', 'r_ankle_roll', 'r_elbow', 'r_f0_j0', 'r_f1_j0', 'r_hip_pitch', 'r_hip_roll', 'r_hip_yaw', 'r_knee', 'r_shoulder_pitch', 'r_shoulder_roll', 'r_shoulder_yaw', 'r_wrist_roll', 'r_wrist_yaw1', 'r_wrist_yaw2', 'waist_lidar', 'waist_pan', 'waist_tilt']
+        self._all_joints = ['head_pan', 'head_tilt', 'l_ankle_pitch', 'l_ankle_roll', 'l_elbow', 'l_f0_j0', 'l_f1_j0', 'l_hip_pitch', 'l_hip_roll', 'l_hip_yaw', 'l_knee', 'l_shoulder_pitch', 'l_shoulder_roll', 'l_shoulder_yaw', 'l_wrist_roll', 'l_wrist_yaw1', 'l_wrist_yaw2', 'r_ankle_pitch', 'r_ankle_roll', 'r_elbow', 'r_f0_j0', 'r_f1_j0', 'r_hip_pitch', 'r_hip_roll', 'r_hip_yaw', 'r_knee', 'r_shoulder_pitch', 'r_shoulder_roll', 'r_shoulder_yaw', 'r_wrist_roll', 'r_wrist_yaw1', 'r_wrist_yaw2', 'waist_lidar', 'waist_pan', 'waist_tilt']
         
         self._sub = rospy.Subscriber("/thor_mang/joint_states_raw", JointState, self._joint_state_cb)
         self._pub = rospy.Publisher("/thor_mang/joint_states", JointState, queue_size=10)
-
+ 
     def _joint_state_cb(self, joint_states):
         for joint in self._all_joints:
             if not joint in joint_states.name:
